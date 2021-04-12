b869f368861de27a5e46c3b5ec2241af9a82ce4a
alessiarocco98 <alessia.rocco@studio.unibo.it>
2021-04-12 16:45:44 +0200

    exercise 1 done

--- src/u06lab/code/Combiner.scala
+++ src/u06lab/code/Combiner.scala
@@ -1,5 +1,7 @@
 package u06lab.code
 
+import u06lab.code.Combiners.{CombinerConcat, CombinerMax, CombinerSum}
+
 /**
   * 1) Implement trait Functions with an object FunctionsImpl such that the code
   * in TryFunctions works correctly.
@@ -9,18 +11,61 @@ trait Functions {
   def sum(a: List[Double]): Double
   def concat(a: Seq[String]): String
   def max(a: List[Int]): Int // gives Int.MinValue if a is empty
+  def combine[A](seq: Seq[A])(implicit combiner: Combiner[A]): A
+}
+
+object Combiners {
+  implicit object CombinerSum extends Combiner[Double]{
+    override def unit: Double = 0.0
+
+    override def combine(a: Double, b: Double): Double = a + b
+  }
+  implicit object CombinerConcat extends Combiner[String] {
+    override def unit: String = ""
+
+    implicit override def combine(a: String, b: String): String = a + b
+  }
+  implicit object CombinerMax extends Combiner[Int] {
+    override def unit: Int = Integer.MIN_VALUE
+
+    implicit override def combine(a: Int, b: Int): Int = Integer.max(a,b)
+  }
 }
 
 object FunctionsImpl extends Functions {
+  /*
+  override def sum(a: List[Double]): Double = {
+    var sum: Double = 0
+    a.foreach(e => sum = sum + e)
+    sum
+  }*/
 
-  override def sum(a: List[Double]): Double = ???
+  /*
+  override def concat(a: Seq[String]): String = {
+    var resultString = ""
+    a.foreach(e => resultString = resultString + e)
+    resultString
+  }*/
 
-  override def concat(a: Seq[String]): String = ???
+  /*
+  override def max(a: List[Int]): Int = {
+    if(a.isEmpty){
+      Integer.MIN_VALUE
+    } else {
+      a.max(Ordering[Int])
+    }
+  }*/
 
-  override def max(a: List[Int]): Int = ???
+  override def sum(a: List[Double]): Double = combine(a)(implicitly[Combiner[Double]])
+  override def concat(a: Seq[String]): String = combine(a)(implicitly[Combiner[String]])
+  override def max(a: List[Int]): Int = combine(a)(implicitly[Combiner[Int]])
+
+  def combine[A](seq: Seq[A])(implicit combiner: Combiner[A]): A = {
+    var c:A = combiner.unit
+    if (seq.nonEmpty) seq.foreach(e => c = combiner.combine(e,c) )
+    c
+  }
 }
-
-
 /*
   * 2) To apply DRY principle at the best,
   * note the three methods in Functions do something similar.
@@ -33,14 +78,14 @@ object FunctionsImpl extends Functions {
   *
   * When all works, note we completely avoided duplications..
  */
-
 trait Combiner[A] {
   def unit: A
   def combine(a: A, b: A): A
 }
 
-object TryFunctions extends App {
+object TryFunctionsComb extends App {
   val f: Functions = FunctionsImpl
+
   println(f.sum(List(10.0,20.0,30.1))) // 60.1
   println(f.sum(List()))                // 0.0
   println(f.concat(Seq("a","b","c")))   // abc
